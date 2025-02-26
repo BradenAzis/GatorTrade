fd092e013f9483f90dba026e20094af5dda890a0
BradenAzis <Bradenkai747@gmail.com>
2025-02-26 18:17:34 -0500

    Finalized Template

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const userRoutes = require("./routes/userRoutes");
+
 require("dotenv").config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use("/users", userRoutes);
+
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,

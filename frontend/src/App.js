bd16496f7f054bfdf4a59d4260935bb8691f4345
BrianTDelaney <briantdel@gmail.com>
2025-04-07 00:19:39 -0400

    Basic about page set-up with placeholder text and image, still needs text-wrapping. Fixed nav bar.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,15 +8,13 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 function App() {
   return (
     <BrowserRouter>
-        <header className="App-logo">
-            <p>
-                GatorTrade
-            </p>
-        </header>
-        <header className="PageButton">
-            <a href="/">Home</a>
-            <a href="/Listings">Listings</a>
-        </header>
+        <div className="NavBar">
+            <div className="App-logo">GatorTrade</div>
+            <div className="PageButton">
+                <a href="/">Home</a>
+                <a href="/Listings">Listings</a>
+            </div>
+        </div>
         <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/listings" element={<Listings />} />
--- frontend/src/HomePage.js
+++ frontend/src/HomePage.js
@@ -1,8 +1,26 @@
 import './App.css'
+import coolimage from './resources/images/importantazis.jpg'
 
 function HomePage(){
     return (
-        <div className="App">
+        <div className="PageContent">
+            <h1 className="ContentHeader">About Us</h1>
+            <div className="ContentParagraph">
+                <p className="ParagraphText">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                    laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt
+                    in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                    laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt
+                    in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                    laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt
+                    in culpa qui officia deserunt mollit anim id est laborum. </p>
+                <img className="ParagraphVisual" src={coolimage} alt="azis"/>
+            </div>
         </div>
     );
 }

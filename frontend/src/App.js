1f981d2d7744ff8e1358ee61753dc4477d9502a6
mlozito <matt.lozito@gmail.com>
2025-04-08 18:02:46 -0400

    media kit update and nav bar changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,29 @@
-
 import './App.css';
 import HomePage from './HomePage';
 import Listings from './Listings';
+import Profile from './Profile';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
+import logo from './resources/images/GatorTradeLogo.png';
 
 
 function App() {
   return (
     <BrowserRouter>
         <div className="NavBar">
-            <div className="App-logo">GatorTrade</div>
+            <div className="App-logo">
+                <a href="/Listings">
+                    <img src={logo} alt={"GatorTrade"}></img>
+                </a>
+            </div>
             <div className="PageButton">
-                <a href="/">Home</a>
-                <a href="/Listings">Listings</a>
+                <a href="/">About</a>
+                <a href="/Profile">Profile</a>
             </div>
         </div>
         <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/listings" element={<Listings />} />
+            <Route path="/profile" element={<Profile />} />
         </Routes>
     </BrowserRouter>
   );
---
+++ frontend/src/Profile.js
@@ -0,0 +1,15 @@
+import './App.css'
+
+function Profile(){
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>
+                    Profile
+                </p>
+            </header>
+        </div>
+    );
+}
+
+export default Profile;

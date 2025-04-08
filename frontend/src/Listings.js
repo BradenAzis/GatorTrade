1fa70cf19f807fa992c008973de4806ac351d2b9
BrianTDelaney <briantdel@gmail.com>
2025-04-08 18:42:37 -0400

    Fixes to search bar styling, font application

--- frontend/src/Listings.js
+++ frontend/src/Listings.js
@@ -3,12 +3,10 @@ import './App.css'
 function Listing(){
     return (
         <div className="PageContent">
-            <h1 className="ContentHeader">Listings</h1>
             <div className="SearchBar">
-                <label for="listinginput"></label>
+                <label></label>
                 <input type="text" id="listinginput"  name="lquery" placeholder="Search for listings..."/>
             </div>
-
             <h3 className="ContentSubHeader">Recently Added</h3>
         </div>
     );

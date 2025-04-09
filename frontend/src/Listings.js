10183e6b7fc0a240c2530c9a2cab30476cd2c59f
mlozito <matt.lozito@gmail.com>
2025-04-09 15:20:03 -0400

    structure for viewing listings.

--- frontend/src/Listings.js
+++ frontend/src/Listings.js
@@ -5,11 +5,23 @@ function Listing(){
         <div className="PageContent">
             <div className="SearchBar">
                 <label></label>
-                <input type="text" id="listinginput"  name="lquery" placeholder="Search for listings..."/>
+                <input type="text" id="listinginput"  name="lquery" placeholder="Search for listings..." onKeyDown={Search}/>
+            </div>
+            <div className="UserListings">
+                <div className="Card">
+                    <div className="Image">Image</div>
+                    <div className="Name">Brand new ipad</div>
+                </div>
             </div>
             <h3 className="ContentSubHeader">Recently Added</h3>
         </div>
     );
+}
+function Search(e){
+    if (e.keyCode === 13) {
+        alert("Looking for " + e.target.value + "?");
+        //filter database based on e
+    }
 }
 
 export default Listing;

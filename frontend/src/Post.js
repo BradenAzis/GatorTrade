836aeb00925fbd8034d139fb6010d7d28a98712f
BrianTDelaney <briantdel@gmail.com>
2025-04-14 17:30:59 -0400

    Basic layout for listing posting

--- frontend/src/Post.js
+++ frontend/src/Post.js
@@ -4,6 +4,25 @@ function Post (){
     return (
         <div className={"PageContent"}>
             <h1 className={"ContentHeader"}>Create a Listing</h1>
+            <h1 className={"InputHeader"}>Title & Description</h1>
+            <div className={"InputField"}>
+                <input type={"text"} name="ltitle" placeholder={"Listing Title"}/>
+            </div>
+            <div className={"ParagraphField"}>
+                <textarea name={"ldesc"} placeholder={"Listing Description"}/>
+            </div>
+            <h1 className={"InputHeader"}>Pricing</h1>
+            <div className={"InlineContent"}>
+                <h1 className={"ParagraphText"}>$</h1>
+                <div className={"InputField"}>
+                    <input type={"text"} name="lprice" placeholder={"0.00"}/>
+                </div>
+            </div>
+            <h1 className={"InputHeader"}>Tags</h1>
+            <div className={"InputField"}>
+                <input type={"text"} name="ltags" placeholder={"Tags"}/>
+            </div>
+            <a className={"Button"} href="#">Post Listing</a>
         </div>
     )
 }

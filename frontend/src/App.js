a0ae661500688fb0002561b124442d5b8a7a9ece
Braden Azis <Bradenkai747@gmail.com>
2025-04-19 15:21:21 -0400

    Brian's App.js changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import LandingPage from './LandingPage'
 import HomePage from './HomePage';
 import Listings from './Listings';
 import Profile from './Profile';
@@ -7,10 +6,39 @@ import Post from './Post';
 import Messages from './Messages'
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import logo from './resources/images/GatorTradeLogo.png';
-import ListingInfo from "./ListingInfo";
+import {useEffect} from "react";
 
 
 function App() {
+
+    const ButtonRefState = ['/Profile', 'http://localhost:5001/auth/google']
+    const ButtonNameState = ['My Profile', 'Login']
+    let index = 1
+
+    const CheckUserState = async () => {
+        const response = await fetch('http://localhost:5001/auth/me', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            credentials: 'include'
+        });
+
+        if (!response.ok) {
+            console.log(response);
+        }
+        else{
+            console.log(response);
+        }
+
+    }
+
+    useEffect(() => {
+
+        CheckUserState()//first execution
+
+    },[]);
+
   return (
     <BrowserRouter>
         <div className="NavBar">
@@ -20,22 +48,18 @@ function App() {
                 </a>
             </div>
             <div className="PageButton">
-
-                <a href="/">About</a>
-                <a href="/Profile">Profile</a>
-                <a href={"/Create"}>Post</a>
+                <a href={ButtonRefState[index]}>{ButtonNameState[index]}</a>
+                <a href={"/Post"}>Post</a>
+                <a href="/About">About</a>
                 <a href="/Messages">Messages</a>
             </div>
         </div>
         <Routes>
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/home" element={<HomePage />} />
-            <Route path="/listings" element={<Listings />} />
+            <Route path="/" element={<Listings />} />
+            <Route path="/about" element={<HomePage />} />
             <Route path="/profile" element={<Profile />} />
-            <Route path="/create" element={<Post />} />
-            <Route path="/listings/:id" element={<ListingInfo />} />
             <Route path="/messages" element={<Messages />} />
-
+            <Route path="/post" element={<Post />} />
         </Routes>
     </BrowserRouter>
   );
